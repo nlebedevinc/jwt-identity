969b20eda26ec937c6872656de2d98823597883e
nlebedevinc <nlebedevme@gmail.com>
2019-12-28 03:31:13 +0300

    Antoher method to write pool query

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,5 @@
 import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi';
-import { Pool } from 'pg';
+import { Pool, QueryResult } from 'pg';
 
 /**
  * Simple function that demonstrates intsert request to psql
@@ -7,18 +7,20 @@ import { Pool } from 'pg';
  * @param name
  * @param password
  */
-function storeCreds(pool: Pool, name: string, password: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        pool.query('INSERT INTO users (name, password) VALUES ($1, $2)', [name, password], (error: Error, result: any) => {
-            if (error) {
-                reject(error);
-            }
-    
-            console.info(`Users creds where added with id: ${result}`);
+function storeCreds(pool: Pool, name: string, password: string): Promise<QueryResult> {
+    // return new Promise((resolve, reject) => {
+    //     pool.query('INSERT INTO users (name, password) VALUES ($1, $2)', [name, password], (error: Error, result: any) => {
+    //         if (error) {
+    //             reject(error);
+    //         } else {
+    //             console.info(`Users creds where added with id: ${result}`);
 
-            resolve(result);
-        });
-    })
+    //             resolve(result);
+    //         }
+    //     });
+    // })
+
+    return pool.query('INSERT INTO users (name, password) VALUES ($1, $2)', [name, password]);
 }
 
 export async function login(_: Request, h: ResponseToolkit): Promise<ResponseObject | Error> {
@@ -28,10 +30,13 @@ export async function login(_: Request, h: ResponseToolkit): Promise<ResponseObj
 
     console.info(`Login: ${login}, Password: ${password}`);
 
+    try {
+        await storeCreds(pool, login, password);
+    } catch (error) {
+        console.error(error.message);
+    }
 
-    const result = await storeCreds(pool, login, password);
-
-    console.info(result);
+    // console.info(result);
 
     // perform auth logic here
     return h.response(_.payload).code(200);
