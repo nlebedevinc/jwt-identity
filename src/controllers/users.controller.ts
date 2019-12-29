c3459f1d4c1bb9e1180d3422912425658619790d
nlebedevinc <nlebedevme@gmail.com>
2019-12-30 00:39:41 +0300

    Provide description, do some poc refactoring

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,12 @@
 import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi';
 import { Pool, QueryResult } from 'pg';
+import { sign } from 'jsonwebtoken';
 
+function generateToken(): string {
+    const token = sign({}, 'private key');
+
+    return token;
+}
 
 // works!
 function storeAlternative(pool: Pool, name: string, password: string): Promise<QueryResult> {
@@ -28,6 +34,19 @@ function storeCreds(pool: Pool, name: string, password: string): Promise<QueryRe
     return pool.query(query, [name, password]);
 }
 
+/**
+ * Login controller functionality
+ * GIVEN a password and login
+ * THEN find a user in the database
+ * IF user not found
+ * THEN unauthorized
+ * THEN validate password
+ * THEN create a JWT token
+ * THEN update session table
+ * THEN return response to the client
+ * @param _
+ * @param h 
+ */
 export async function login(_: Request, h: ResponseToolkit): Promise<ResponseObject | Error> {
     // get payload information
     const { login, password } = _.payload as any;
