efdf4aa5c041efb331b90741ab0f476b9e9b3090
nlebedevinc <nlebedevme@gmail.com>
2020-01-07 00:51:14 +0300

    Update controller implementation

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,7 @@
 import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi';
 import { Pool, QueryResult } from 'pg';
-// import { sign } from 'jsonwebtoken';
+import { badImplementation, unauthorized } from '@hapi/boom';
+import { sign } from 'jsonwebtoken';
 
 // function generateToken(): string {
 //     const token = sign({}, 'private key');
@@ -30,13 +31,25 @@ import { Pool, QueryResult } from 'pg';
  * @param password
  */
 function storeCreds(pool: Pool, name: string, password: string): Promise<QueryResult> {
-    const query = 'INSERT INTO users (name, password) VALUES ($1, $2)';
+    const query = 'INSERT INTO sessions (name, password) VALUES ($1, $2)';
     return pool.query(query, [name, password]);
 }
 
 function findUserByLogin(pool: Pool, name: string): Promise<QueryResult> {
     const query = 'SELECT * from users WHERE name = $1';
     return pool.query(query, [name]);
+}
+
+function generateToken(user: any): string {
+    const current = new Date();
+
+    return sign({
+        'iis': 'Identity service', // service name
+        'iat': Math.floor(current.getTime() / 1000),
+        'exp': Math.floor(current.getTime() / 1000),
+        'aud': 'http://localhost:3000', // server url
+        'sub': user.id,
+    }, 'private_key');
 }
 
 /**
@@ -50,33 +63,32 @@ function findUserByLogin(pool: Pool, name: string): Promise<QueryResult> {
  * THEN update session table
  * THEN return response to the client
  * @param _
- * @param h 
+ * @param h
  */
-export async function login(_: Request, h: ResponseToolkit): Promise<ResponseObject | Error> {
-    // get payload information
+export async function login(_: Request, h: ResponseToolkit): Promise<ResponseObject | Boom> {
     const { login, password } = _.payload as any;
-    const { pool } = _.server.app as any;
-
-    // find user in the table
-    let user;
-    try {
-        user = await findUserByLogin(pool, login);
-    } catch (error) {
-        console.log(error);
-    }
-
-    console.log(user);
-
-    console.info(`Login: ${login}, Password: ${password}`);
 
     try {
+        const { pool } = _.server.app as any;
+
+        const user: any = await findUserByLogin(pool, login);
+
+        if (!user) {
+            return unauthorized('User does not exist');
+        }
+
+        if (!user.validatePassword(password)) {
+            return unauthorized('Password is invalid');
+        }
+
+        const token = generateToken(user);
+
         await storeCreds(pool, login, password);
+
+        return h.response(token).header('X-Access-Token', token).code(200);
+
     } catch (error) {
-        console.error(error.message);
+        console.error(error);
+        return badImplementation(error.message, { error });
     }
-
-    // console.info(result);
-
-    // perform auth logic here
-    return h.response(_.payload).code(200);
 }
